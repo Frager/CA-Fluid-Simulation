9caa7172ae52266fa404588fafffe43c78c61c75
Christian Heintz <Hychris@web.de>
2017-04-05 16:38:59 +0200

    latest version of MultiContentWithFlagsRule

--- Cellular Automaton on GPU/Assets/Scripts/CPU Fluid CA/MultiContentWithFlagsRule.cs
+++ Cellular Automaton on GPU/Assets/Scripts/CPU Fluid CA/MultiContentWithFlagsRule.cs
@@ -21,103 +21,106 @@ namespace CPUFluid
                         int contentChange = 0;
 
 
-                        //******************************************************************************
-                        //swap 1 high with 1 low density content vertically
-                        //******************************************************************************
+                       
+                        if (updateCycle % 2 == 1)
+                        { 
+                            //******************************************************************************
+                            //swap 1 high with 1 low density content vertically
+                            //******************************************************************************
 
-                        //if content > 1 and bottom cell is full, add 1 lowest dense content from bottom and delete 1 of highest density
-                        if (y > 0 && currentGen[x, y - 1, z].getVolume() == maxVolume && currentGen[x, y, z].getVolume() > 1)
-                        {
-                            int lightestIdBottom = currentGen[x, y - 1, z].getLightestContent();
-                            int heaviestIdCurrent = currentGen[x, y, z].getHeaviestContent();
-                            if(lightestIdBottom != heaviestIdCurrent)
+                            //if content > 1 and bottom cell is full, add 1 lowest dense content from bottom and delete 1 of highest density
+                            if (y > 0 && currentGen[x, y - 1, z].getVolume() == maxVolume && currentGen[x, y, z].getVolume() > 1)
                             {
-                                newGen[x, y, z].addContent(1, lightestIdBottom);
-                                newGen[x, y, z].takeContent(1, heaviestIdCurrent);
-                            }
-                        }
-                        //if content full and top cell has content > 1 ,add 1 heighes dense content from top and delete 1 of lowest density
-                        if (y < currentGen.GetLength(1) - 1 && currentGen[x, y, z].getVolume() == maxVolume && currentGen[x, y + 1, z].getVolume() > 1)
-                        {
-                            int lightestIdCurrent = currentGen[x, y, z].getLightestContent();
-                            int heaviestIdTop = currentGen[x, y + 1, z].getHeaviestContent();
-                            if (lightestIdCurrent != heaviestIdTop)
-                            {
-                                newGen[x, y, z].addContent(1, heaviestIdTop);
-                                newGen[x, y, z].takeContent(1, lightestIdCurrent);
-                            }
-                        }
-
-                        //******************************************************************************
-                        //push content up if volume > maxVolume
-                        //******************************************************************************
-
-                        //if volume > maxVolume delete 1 content with lowest density
-                        if (currentGen[x, y, z].getVolume() > maxVolume)
-                        {
-                            for (int id = 0; id < currentGen[x, y, z].content.Length; id++)
-                            {
-                                if (currentGen[x, y, z].content[id] > 0)
+                                int lightestIdBottom = currentGen[x, y - 1, z].getLightestContent();
+                                int heaviestIdCurrent = currentGen[x, y, z].getHeaviestContent();
+                                if (lightestIdBottom != heaviestIdCurrent)
                                 {
-                                    newGen[x, y, z].takeContent(1, id);
-                                    break;
+                                    newGen[x, y, z].addContent(1, lightestIdBottom);
+                                    newGen[x, y, z].takeContent(1, heaviestIdCurrent);
+                                }
+                            }
+                            //if content full and top cell has content > 1 ,add 1 heighes dense content from top and delete 1 of lowest density
+                            if (y < currentGen.GetLength(1) - 1 && currentGen[x, y, z].getVolume() == maxVolume && currentGen[x, y + 1, z].getVolume() > 1)
+                            {
+                                int lightestIdCurrent = currentGen[x, y, z].getLightestContent();
+                                int heaviestIdTop = currentGen[x, y + 1, z].getHeaviestContent();
+                                if (lightestIdCurrent != heaviestIdTop)
+                                {
+                                    newGen[x, y, z].addContent(1, heaviestIdTop);
+                                    newGen[x, y, z].takeContent(1, lightestIdCurrent);
+                                }
+                            }
+
+                            //******************************************************************************
+                            //push content up if volume > maxVolume
+                            //******************************************************************************
+
+                            ////if volume > maxVolume delete 1 content with lowest density
+                            //if (currentGen[x, y, z].getVolume() > maxVolume)
+                            //{
+                            //    for (int id = 0; id < currentGen[x, y, z].content.Length; id++)
+                            //    {
+                            //        if (currentGen[x, y, z].content[id] > 0)
+                            //        {
+                            //            newGen[x, y, z].takeContent(1, id);
+                            //            break;
+                            //        }
+                            //    }
+                            //}
+                            ////if bottom cell volume > maxVolume add 1 content from it with lowest density
+                            //if (y > 0 && currentGen[x, y - 1, z].getVolume() > maxVolume)
+                            //{
+                            //    for (int id = 0; id < currentGen[x, y - 1, z].content.Length; id++)
+                            //    {
+                            //        if (currentGen[x, y - 1, z].content[id] > 0)
+                            //        {
+                            //            newGen[x, y, z].addContent(1, id);
+                            //            break;
+                            //        }
+                            //    }
+                            //}
+
+                            //******************************************************************************
+                            //fill bottom cell up as much as possible
+                            //******************************************************************************
+
+                            //delete as much as bottom cell can take
+                            if (y > 0 && currentGen[x, y - 1, z].getVolume() < maxVolume && currentGen[x, y, z].getVolume() != 0)
+                            {
+                                // contentChange is max amount that can be passed down to botom cell
+                                contentChange = Mathf.Min(maxVolume - currentGen[x, y - 1, z].getVolume(), currentGen[x, y, z].getVolume());
+                                //pass down (delete) contentChange amount of the heaviest contents
+                                for (int id = currentGen[x, y, z].content.Length - 1; id >= 0; --id)
+                                {
+                                    if (contentChange == 0) break;
+                                    int content = currentGen[x, y, z].content[id];
+                                    while (content > 0 && contentChange > 0)
+                                    {
+                                        --content;
+                                        --contentChange;
+                                        newGen[x, y, z].takeContent(1, id);
+                                    }
+                                }
+                            }
+                            //take as much as possible from top cell
+                            if (y < currentGen.GetLength(1) - 1 && currentGen[x, y, z].getVolume() < maxVolume && currentGen[x, y + 1, z].getVolume() != 0)
+                            {
+                                //contentChange is max amount that can be taken from top cell
+                                contentChange = Mathf.Min(maxVolume - currentGen[x, y, z].getVolume(), currentGen[x, y + 1, z].getVolume());
+                                //take contentChange amount from heaviest contents from top cell
+                                for (int id = currentGen[x, y + 1, z].content.Length - 1; id >= 0; --id)
+                                {
+                                    if (contentChange == 0) break;
+                                    int content = currentGen[x, y + 1, z].content[id];
+                                    while (content > 0 && contentChange > 0)
+                                    {
+                                        --content;
+                                        --contentChange;
+                                        newGen[x, y, z].addContent(1, id);
+                                    }
                                 }
                             }
                         }
-                        //if bottom cell volume > maxVolume add 1 content from it with lowest density
-                        if (y > 0 && currentGen[x, y - 1, z].getVolume() > maxVolume)
-                        {
-                            for (int id = 0; id < currentGen[x, y - 1, z].content.Length; id++)
-                            {
-                                if (currentGen[x, y - 1, z].content[id] > 0)
-                                {
-                                    newGen[x, y, z].addContent(1, id);
-                                    break;
-                                }
-                            }
-                        }
-
-                        //******************************************************************************
-                        //fill bottom cell up as much as possible
-                        //******************************************************************************
-
-                        //delete as much as bottom cell can take
-                        if (y > 0 && currentGen[x, y - 1, z].getVolume() < maxVolume && currentGen[x, y, z].getVolume() != 0 )
-                        {
-                            // contentChange is max amount that can be passed down to botom cell
-                            contentChange = Mathf.Min(maxVolume - currentGen[x, y - 1, z].getVolume(), currentGen[x, y, z].getVolume());
-                            //pass down (delete) contentChange amount of the heaviest contents
-                            for (int id = currentGen[x, y, z].content.Length - 1; id >= 0; --id)
-                            {
-                                if (contentChange == 0) break;
-                                int content = currentGen[x, y, z].content[id];
-                                while (content > 0 && contentChange > 0)
-                                {
-                                    --content;
-                                    --contentChange;
-                                    newGen[x, y, z].takeContent(1, id);
-                                }
-                            }
-                        }
-                        //take as much as possible from top cell
-                        if (y < currentGen.GetLength(1) - 1 && currentGen[x, y, z].getVolume() < maxVolume && currentGen[x, y + 1, z].getVolume() != 0)
-                        {
-                            //contentChange is max amount that can be taken from top cell
-                            contentChange = Mathf.Min(maxVolume - currentGen[x, y, z].getVolume(), currentGen[x, y + 1, z].getVolume());
-                            //take contentChange amount from heaviest contents from top cell
-                            for (int id = currentGen[x, y + 1, z].content.Length - 1; id >= 0; --id)
-                            {
-                                if (contentChange == 0) break;
-                                int content = currentGen[x, y + 1, z].content[id];
-                                while (content > 0 && contentChange > 0)
-                                {
-                                    --content;
-                                    --contentChange;
-                                    newGen[x, y, z].addContent(1, id);
-                                }
-                            }
-                        }
-
                         //******************************************************************************
                         // if has not passed down horizontal update
                         //******************************************************************************
@@ -132,6 +135,8 @@ namespace CPUFluid
                             {
                                 if (x < currentGen.GetLength(0) - 1)
                                 {
+                                    //note for optimizing:
+                                    //give for cell[x,y,z] == take for cell[x+1,y,z]
                                     //int give = ([x] - [x + 1]) / (2 * viscosity)
                                     int give = (int)(((float)(currentGen[x, y, z].content[id] - currentGen[x + 1, y, z].content[id])) / (2f * elements[id].viscosity));
                                     if (give > 0)
@@ -148,7 +153,7 @@ namespace CPUFluid
                                     }
                                 }
                             }
-                            if (updateCycle == 1)
+                            if (updateCycle == 2)
                             {
                                 if (x < currentGen.GetLength(0) - 1)
                                 {
@@ -167,7 +172,7 @@ namespace CPUFluid
                                     }
                                 }
                             }
-                            if (updateCycle == 2)
+                            if (updateCycle == 4)
                             {
                                 if (z < currentGen.GetLength(2) - 1)
                                 {
@@ -186,7 +191,7 @@ namespace CPUFluid
                                     }
                                 }
                             }
-                            if (updateCycle == 3)
+                            if (updateCycle == 6)
                             {
                                 if (z < currentGen.GetLength(2) - 1)
                                 {
@@ -209,7 +214,7 @@ namespace CPUFluid
                     }
                 }
             }
-            updateCycle = (updateCycle + 1) % 4;
+            updateCycle = (updateCycle + 1) % 8;
         }
         
         //return true if neighbor has less of element than current Cell and can take more content
